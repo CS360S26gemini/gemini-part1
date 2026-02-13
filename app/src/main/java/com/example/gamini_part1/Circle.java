a3475945eb0611bb9cea1e66b59992082951acd4
SJK159 <sjkhan.q111@gmail.com>
2026-02-14 01:11:17 +0500

    Added circle class

---
+++ app/src/main/java/com/example/gamini_part1/Circle.java
@@ -0,0 +1,10 @@
+package com.example.gamini_part1;
+
+public class Circle extends Shape {
+    private int radius;
+
+    public Circle(int x, int y, int radius) {
+        super(x, y);
+        this.radius = radius;
+    }
+}
