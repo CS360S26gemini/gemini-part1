e84995b1ea1f1075817f6e804c9b859e4af7c207
SJK159 <sjkhan.q111@gmail.com>
2026-02-17 12:25:28 +0500

    Correction - removing color from main branch
    Removed default color assignment from Shape class.

--- app/src/main/java/com/example/gamini_part1/Shape.java
+++ app/src/main/java/com/example/gamini_part1/Shape.java
@@ -3,7 +3,6 @@ package com.example.gamini_part1;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "blue";
 
     public Shape(int x, int y) {
         this.x = x;
