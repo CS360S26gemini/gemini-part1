9a66d7236540f3d719fbedad1917817ba6b420d3
27100218 <27100218@lums.edu.pk>
2026-02-17 14:33:20 +0500

    color set

--- app/src/main/java/com/example/gamini_part1/Shape.java
+++ app/src/main/java/com/example/gamini_part1/Shape.java
@@ -3,7 +3,7 @@ package com.example.gamini_part1;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "black";
+    protected String color = "pink";
 
     public Shape(int x, int y) {
         this.x = x;
