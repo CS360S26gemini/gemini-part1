123696e11427710d7d0ee4b1cbec9a45fbb48160
hussainbaqar <hbaqar91@gmail.com>
2026-02-17 11:02:08 +0500

    color

--- app/src/main/java/com/example/gamini_part1/Shape.java
+++ app/src/main/java/com/example/gamini_part1/Shape.java
@@ -3,7 +3,7 @@ package com.example.gamini_part1;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "blue";
+    protected String color = "red";
 
     public Shape(int x, int y) {
         this.x = x;
